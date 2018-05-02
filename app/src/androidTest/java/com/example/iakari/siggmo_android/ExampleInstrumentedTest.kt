ebab650981014f9c4524c3b95ea3151b0b23846a
takayamaroso <kd1242223@st.kobedenshi.ac.jp>
2018-05-02 15:58:56 +0900

    first commit

---
+++ app/src/androidTest/java/com/example/iakari/siggmo_android/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.example.iakari.siggmo_android
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.example.iakari.siggmo_android", appContext.packageName)
+    }
+}
