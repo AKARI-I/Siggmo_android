34e3e982a5151c58b87ed3f756511e9491193eed
AKARI-I <i.akari.1122@gmail.com>
2018-05-31 13:38:34 +0900

    add other column in SiggmoDB

--- app/src/main/java/com/example/iakari/siggmo_android/SiggmoDB.kt
+++ app/src/main/java/com/example/iakari/siggmo_android/SiggmoDB.kt
@@ -12,10 +12,17 @@ import java.util.*
 // openをとると継承ができなくなるらしい
 open class SiggmoDB (
     // キー値をランダムで決める(とりあえず)
-    @PrimaryKey open var id : String = UUID.randomUUID().toString(),
+        @PrimaryKey open var id : String = UUID.randomUUID().toString(),
 
     // 多分このへんが項目名
-    @Required
-        open var music_name : String = "",
-        open var music_phonetic : String = ""
+        @Required
+        open var music_name     : String = "",  // 曲名
+        open var music_phonetic : String = "",  // 曲名の読み仮名
+        open var singer_name    : String = "",  // 歌手名
+        open var singer_phonetic: String = "",  // 歌手名の読み仮名
+        open var first_line     : String = "",  // 歌いだし
+        open var proper_key     : Int = 0,      // 適正キー
+        open var movie_link     : String = "",  // 動画のリンク
+        open var score          : Float = 0F,   // 採点結果
+        open var free_memo      : String = ""  // 自由記入欄
 ) : RealmObject(){}
