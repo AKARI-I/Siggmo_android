c985b64a7e7ce954cd92af0b439a4f7138f041c8
AKARI-I <i.akari.1122@gmail.com>
2018-06-27 10:50:24 +0900

    Add quaryById method

--- app/src/main/java/com/example/iakari/siggmo_android/DetailActivity.kt
+++ app/src/main/java/com/example/iakari/siggmo_android/DetailActivity.kt
@@ -2,17 +2,41 @@ package com.example.iakari.siggmo_android
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import io.realm.Realm
+import io.realm.RealmConfiguration
 import kotlinx.android.synthetic.main.activity_detail.*
 
-
 class DetailActivity : AppCompatActivity() {
+    lateinit var mRealm: Realm
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
+        /*-------------------- Realm --------------------*/
+        // Realmのセットアップ
+        Log.d("TAG", "Realmセットアップ開始(DetailActivity)")
+        Realm.init(this)
+        val realmConfig = RealmConfiguration.Builder()
+                .deleteRealmIfMigrationNeeded()
+                .build()
+        mRealm = Realm.getInstance(realmConfig)
+        Log.d("TAG", "Realmセットアップ終了(DetailActivity)")
+
         // 受け取ったIDをTextViewで表示
         val tapid = intent.getStringExtra("TapID")
-        testTextView.text = tapid
+        Log.d("TAG", "clear intent")
+        val record = quaryById(tapid)
+        Log.d("TAG", "clear quaryById method")
+        testTextView.text = record.toString()
+        Log.d("TAG", "clear textview.text set")
+    }
+
+    // 渡されたidからデータベースを検索してレコードを返す
+    fun quaryById(id: String): SiggmoDB? {
+        return mRealm.where(SiggmoDB::class.java)
+                .equalTo("id", id)
+                .findFirst()
     }
 }
