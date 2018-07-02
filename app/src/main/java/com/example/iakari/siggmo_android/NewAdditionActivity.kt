0863d5b7870a859c5e970a8e2ab899061f4a76d4
AKARI-I <i.akari.1122@gmail.com>
2018-07-02 18:55:14 +0900

    Add Realm setup

--- app/src/main/java/com/example/iakari/siggmo_android/NewAdditionActivity.kt
+++ app/src/main/java/com/example/iakari/siggmo_android/NewAdditionActivity.kt
@@ -2,11 +2,25 @@ package com.example.iakari.siggmo_android
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import io.realm.Realm
+import io.realm.RealmConfiguration
 
 class NewAdditionActivity : AppCompatActivity() {
+    lateinit var mRealm: Realm
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_addition)
+
+        /*-------------------- Realm --------------------*/
+        // Realmのセットアップ
+        Log.d("TAG", "Realmセットアップ開始(NewAdditionActivity)")
+        Realm.init(this)
+        val realmConfig = RealmConfiguration.Builder()
+                .deleteRealmIfMigrationNeeded()
+                .build()
+        mRealm = Realm.getInstance(realmConfig)
+        Log.d("TAG", "Realmセットアップ終了(NewAdditionActivity)")
     }
 }
