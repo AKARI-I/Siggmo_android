c09148e2f9be0efa745cc80f48e55c346470bb0b
AKARI-I <i.akari.1122@gmail.com>
2018-07-02 19:05:16 +0900

    Add setOnClickListener in saveButton

--- app/src/main/java/com/example/iakari/siggmo_android/NewAdditionActivity.kt
+++ app/src/main/java/com/example/iakari/siggmo_android/NewAdditionActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import io.realm.Realm
 import io.realm.RealmConfiguration
+import kotlinx.android.synthetic.main.activity_new_addition.*
 import java.util.*
 
 class NewAdditionActivity : AppCompatActivity() {
@@ -36,15 +37,15 @@ class NewAdditionActivity : AppCompatActivity() {
         Log.d("TAG", "Realmセットアップ終了(NewAdditionActivity)")
 
         /* 保存ボタンがクリックされたらレコードを追加する */
-        // createテスト(テスト用レコードの追加)
         // ※ここではテスト用データを事前に宣言してレコードを作成
-
-        Log.d("TAG", "createメソッド開始")
-        create(MName[0], MPhonetic[0], SName[0], SPhonetic[0], FLine[0], PKey[0],
-                MLink[0], Score[0], FMemo[0])
-        create(MName[1], MPhonetic[1], SName[1], SPhonetic[1], FLine[1], PKey[1],
-                MLink[1], Score[1], FMemo[1])
-        Log.d("TAG", "createメソッド終了")
+        saveButon.setOnClickListener {
+            Log.d("TAG", "createメソッド開始")
+            create(MName[0], MPhonetic[0], SName[0], SPhonetic[0], FLine[0], PKey[0],
+                    MLink[0], Score[0], FMemo[0])
+            create(MName[1], MPhonetic[1], SName[1], SPhonetic[1], FLine[1], PKey[1],
+                    MLink[1], Score[1], FMemo[1])
+            Log.d("TAG", "createメソッド終了")
+        }
     }
 
 
