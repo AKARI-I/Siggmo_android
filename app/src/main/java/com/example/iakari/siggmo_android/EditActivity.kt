16c5142fa9835a464ca60162ea34804b7079c019
KawasakiHachi <kd1221092@st.kobedenshi.ac.jp>
2018-07-23 10:21:52 +0900

    for pull

--- app/src/main/java/com/example/iakari/siggmo_android/EditActivity.kt
+++ app/src/main/java/com/example/iakari/siggmo_android/EditActivity.kt
@@ -2,11 +2,40 @@ package com.example.iakari.siggmo_android
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import io.realm.Realm
+import io.realm.RealmConfiguration
+import kotlinx.android.synthetic.main.activity_edit.*
+
 
 class EditActivity : AppCompatActivity() {
+    lateinit var mRealm: Realm
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
+
+        // Realmのセットアップ
+        Log.d("TAG", "Realmセットアップ開始")
+        Realm.init(this)
+        val realmConfig = RealmConfiguration.Builder()
+                .deleteRealmIfMigrationNeeded()
+                .build()
+        mRealm = Realm.getInstance(realmConfig)
+        Log.d("TAG", "Realmセットアップ終了")
+
+        val tapid = intent.getStringExtra("TapID")
+        val record = quaryById(tapid)
     }
+
+
+
+
+    fun quaryById(id: String): SiggmoDB? {
+        return mRealm.where(SiggmoDB::class.java)
+                .equalTo("id", id)
+                .findFirst()
+    }
+
 }
+
