e95f6cbd8fca02b500ceff719c9ac267851c6ac8
KawasakiHachi <kd1221092@st.kobedenshi.ac.jp>
2018-07-23 12:00:56 +0900

    表示項目をすべて追加　labelforについてバグが出ているので調べて修正の必要あり

--- app/src/main/java/com/example/iakari/siggmo_android/EditActivity.kt
+++ app/src/main/java/com/example/iakari/siggmo_android/EditActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import io.realm.Realm
 import io.realm.RealmConfiguration
+import kotlinx.android.synthetic.main.activity_detail.*
 import kotlinx.android.synthetic.main.activity_edit.*
 
 
@@ -14,7 +15,7 @@ class EditActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
-
+/*
         // Realmのセットアップ
         Log.d("TAG", "Realmセットアップ開始")
         Realm.init(this)
@@ -26,16 +27,29 @@ class EditActivity : AppCompatActivity() {
 
         val tapid = intent.getStringExtra("TapID")
         val record = quaryById(tapid)
+
+        if (record != null) {
+            music_name.text = record.music_name
+            music_phonetic.text = record.music_phonetic
+            singer_name.text = record.singer_name
+            singer_phonetic.text = record.singer_phonetic
+            first_line.text = record.first_line
+            //proper_key.text = record.proper_key
+            movie_link.text = record.movie_link
+            //score. = record.score
+            free_memo.text = record.free_memo
+
+        }
+        */
     }
 
 
-
-
+    /*
     fun quaryById(id: String): SiggmoDB? {
         return mRealm.where(SiggmoDB::class.java)
                 .equalTo("id", id)
                 .findFirst()
     }
+    */
 
 }
-
