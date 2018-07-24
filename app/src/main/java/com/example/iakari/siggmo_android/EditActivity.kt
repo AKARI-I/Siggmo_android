c85bf2dcb91b4892dbbf365ea3db14df75a2d646
KawasakiHachi <kd1221092@st.kobedenshi.ac.jp>
2018-07-24 10:27:34 +0900

    kt側から値を渡す処理を書いてある

--- app/src/main/java/com/example/iakari/siggmo_android/EditActivity.kt
+++ app/src/main/java/com/example/iakari/siggmo_android/EditActivity.kt
@@ -3,10 +3,13 @@ package com.example.iakari.siggmo_android
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import io.realm.Realm
 import io.realm.RealmConfiguration
 import kotlinx.android.synthetic.main.activity_detail.*
 import kotlinx.android.synthetic.main.activity_edit.*
+import android.widget.EditText
+import android.widget.TextView
 
 
 class EditActivity : AppCompatActivity() {
@@ -15,7 +18,11 @@ class EditActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
-/*
+
+ //       val editText = findViewById<EditText>(android.R.id.music_name_edit)
+        val edit = m_name_edit.text.toString()
+        m_name_edit.setText("==========")
+
         // Realmのセットアップ
         Log.d("TAG", "Realmセットアップ開始")
         Realm.init(this)
@@ -40,16 +47,16 @@ class EditActivity : AppCompatActivity() {
             free_memo.text = record.free_memo
 
         }
-        */
+
     }
 
 
-    /*
+
     fun quaryById(id: String): SiggmoDB? {
         return mRealm.where(SiggmoDB::class.java)
                 .equalTo("id", id)
                 .findFirst()
     }
-    */
+
 
 }
