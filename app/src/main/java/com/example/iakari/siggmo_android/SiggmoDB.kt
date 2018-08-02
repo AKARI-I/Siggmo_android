e03b096a465c99937e3f9d907547af81b33f86d8
AKARI-I <i.akari.1122@gmail.com>
2018-08-02 15:13:48 +0900

    Add music_id in scoreResultDB

--- app/src/main/java/com/example/iakari/siggmo_android/SiggmoDB.kt
+++ app/src/main/java/com/example/iakari/siggmo_android/SiggmoDB.kt
@@ -17,7 +17,7 @@ open class SiggmoDB (
     // @Requiredでnull指定を禁止
         @Required
         open var music_name     : String = "",  // 曲名
-        open var music_phonetic : String  = "",  // 曲名の読み仮名
+        open var music_phonetic : String  = "", // 曲名の読み仮名
         open var singer_name    : String = "",  // 歌手名
         open var singer_phonetic: String = "",  // 歌手名の読み仮名
         open var first_line     : String = "",  // 歌いだし
@@ -25,7 +25,7 @@ open class SiggmoDB (
         open var movie_link     : String = "",  // 動画のリンク
         open var free_memo      : String = "",  // 自由記入欄
         open var list_id        : String = "",  // リスト照合用のID
-        open var score_id       : String = ""   // 採点結果照合用のID
+        open var score_id       : String = ""   // 採点結果照合用ID(同曲でも別々に付与、消すかも)
 ) : RealmObject(){}
 
 // openをとると継承ができなくなるらしい
@@ -38,6 +38,7 @@ open class ListDB (
 open class ScoreResultDB(
         @PrimaryKey open var score_id : String = UUID.randomUUID().toString(),
         //@Required
+        open var music_id : String = "",        // どの曲の採点結果かを保存
         open var score : Float = 0F
 
 ) : RealmObject(){}
