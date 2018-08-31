343567ba5e36699f8a62cde1579bceaef74fe306
AKARI-I <i.akari.1122@gmail.com>
2018-08-31 21:18:12 +0900

    Delete log in DetailActivity

--- app/src/main/java/com/example/iakari/siggmo_android/DetailActivity.kt
+++ app/src/main/java/com/example/iakari/siggmo_android/DetailActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.text.InputType
-import android.util.Log
 import android.view.KeyEvent
 import android.widget.Button
 import android.widget.EditText
@@ -24,8 +23,6 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
-        Log.d("TAG", "start DetailActivity")
-
         /*-------------------- Realm --------------------*/
         Realm.init(this)
         val realmConfig = RealmConfiguration.Builder()
@@ -39,26 +36,19 @@ class DetailActivity : AppCompatActivity() {
         val record = quaryById(tapid)
         val s_record = readScore(record!!.id)
 
-        /*s_record.forEach {
-            Log.d("s_record", "\nscore = ${it.score}\nreq_date${it.reg_data}")
-        }
-        Log.d("s_record", "\n===============================\n")*/
-
         // レコードが返されたら曲名を表示
-        //if (s_record != null) {
-            music_name.text      = record.music_name
-            music_phonetic.text  = record.music_phonetic
-            singer_name.text     = record.singer_name
-            singer_phonetic.text = record.singer_phonetic
-            first_line.text      = record.first_line
-            singing_level.text   = record.singing_level.toString()
-            proper_key.text      = record.proper_key
-            movie_link.text      = record.movie_link
-            score.text           = checkScore(s_record.max("score") as Float?).toString()
-            free_memo.text       = record.free_memo
-            last_update.text     = s_record.last()!!.reg_data
-        //}
-        Log.d("music_count", "read ${record.music_count}")
+        music_name.text      = record.music_name
+        music_phonetic.text  = record.music_phonetic
+        singer_name.text     = record.singer_name
+        singer_phonetic.text = record.singer_phonetic
+        first_line.text      = record.first_line
+        singing_level.text   = record.singing_level.toString()
+        proper_key.text      = record.proper_key
+        movie_link.text      = record.movie_link
+        score.text           = checkScore(s_record.max("score") as Float?).toString()
+        free_memo.text       = record.free_memo
+        last_update.text     = s_record.last()!!.reg_data
+
         score_detail.setOnClickListener {dialogRun(tapid, record.music_count)}
 
         /*------------------- Button --------------------*/
@@ -71,7 +61,6 @@ class DetailActivity : AppCompatActivity() {
             //新しくアクティビティを開く
             startActivity(intent)
         }
-        Log.d("TAG", "finish DetailActivity")
     }
 
     // 標準Backkeyの遷移先変更
@@ -112,7 +101,6 @@ class DetailActivity : AppCompatActivity() {
                                 .equalTo("score_id", getData[0]!!.score_id)
                                 .findAll()
                         mRealm.executeTransaction(Realm.Transaction {
-                            Log.d("TAG", "in realm delete process")
                             results.deleteFromRealm(0)
                             results.deleteLastFromRealm()
                         })
@@ -133,7 +121,6 @@ class DetailActivity : AppCompatActivity() {
     // 渡されたidからデータベースを検索して曲の情報を返す
     // select * from SiggmoDB where id = idと同じ意味
     fun quaryById(id: String): SiggmoDB? {
-        Log.d("TAG", "quaryById(DetailActivity)")
         return mRealm.where(SiggmoDB::class.java)
                 .equalTo("id", id)
                 .findFirst()
@@ -145,7 +132,6 @@ class DetailActivity : AppCompatActivity() {
 
     // scoreを参照する
     fun quaryByScore(m_id: String): ScoreResultDB? {
-        Log.d("TAG", "quaryByScore(DetailActivity)")
         return mRealm.where(ScoreResultDB::class.java)
                 .equalTo("music_id", m_id)
                 .findFirst()
@@ -172,8 +158,7 @@ class DetailActivity : AppCompatActivity() {
 
             val date = "$year/$month/$day/$hour:$minute:$second"    // 年/月/日/時:分:秒
 
-            Log.d("music_count", "${siggmoDB!!.music_count}")
-            siggmoDB.music_count = siggmoDB.music_count + 1
+            siggmoDB!!.music_count = siggmoDB.music_count + 1
             scoreResultDB.music_id = musicId
             scoreResultDB.score = score
             scoreResultDB.reg_data = date
